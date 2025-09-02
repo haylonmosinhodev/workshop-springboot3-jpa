30b7f1e4639404b18b19b1b2fabbe42cc6ec577a
Haylon Mosinho <haylonmosinho.dev@gmail.com>
2025-09-02 12:20:08 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -14,6 +14,8 @@ import com.educandoweb.course.resources.exceptions.DatabaseException;
 import com.educandoweb.course.resources.exceptions.ResourceExceptionHandler;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 
 @Service
 public class UserService {
@@ -60,9 +62,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
